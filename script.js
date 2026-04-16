928cb92a68683a54751c0bd184a88b2a5ca16b62
oliviaflorence-jpg <olivia.florence@sjsu.edu>
2026-04-15 23:53:40 -0700

    pink floyd tings :3

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const AUDIO_OVERRIDES_BY_ID = {
   "img-6050": "./assets/audio/閉店後の滞在.mp3",
   "img-6051": "./assets/audio/Izzy Perri & Sun City - Next to You.mp3",
   "img-6045": "./assets/audio/The Equaliser (Not Alone).mp3",
+  "img-6047": "./assets/audio/Comfortably Numb.mp3",
   "img-6046": "./assets/audio/OutKast -  ATLiens  (HQ).mp3",
 };
 const AUDIO_OVERRIDES_BY_IMAGE = {
@@ -21,6 +22,7 @@ const AUDIO_OVERRIDES_BY_IMAGE = {
   "img_6050.jpg": "./assets/audio/閉店後の滞在.mp3",
   "img_6051.jpg": "./assets/audio/Izzy Perri & Sun City - Next to You.mp3",
   "img_6045.jpg": "./assets/audio/The Equaliser (Not Alone).mp3",
+  "img_6047.jpg": "./assets/audio/Comfortably Numb.mp3",
   "img_6053.jpg": "./assets/audio/Kiss - Turn On The Night (Remastered).mp3",
   "magik_touch_logo.jpg": "./assets/audio/Magic Touch.mp3",
 };
